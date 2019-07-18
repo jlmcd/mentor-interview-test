52ee8da5e9ccb35c564d3b10a7406d9dc7603b3d
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-07-18 14:51:39 -0600

    starting point

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Header from './components/Header'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
